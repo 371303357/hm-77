1890a1967905c69c1a49a7b97a63b548727b8dbc
Lyons <283127368@qq.com>
2019-08-02 22:15:45 +0800

    首页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,20 @@ import VueRouter from 'vue-router'
 import Login from '@/view/login'
 // 引入首页
 import Home from '@/view/home'
+// 引入欢迎页
+import Welcome from '@/view/welcome'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'home', component: Home }
+    {
+      path: '/',
+      component: Home,
+      children: [
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    }
   ]
 })
 export default router
--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,10 +1,128 @@
 <template>
-  <router-view></router-view>
+  <el-container class="warper">
+    <el-aside :width="isCollapse?'64px':'200px'">
+      <div class="logo" :class="{miniLogo:isCollapse}"></div>
+      <el-menu
+        :collapse="isCollapse"
+        :collapse-transition="false"
+        default-active="1"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-edit"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-document"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span class="el-icon-s-fold" @click="toggleMenu()"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <img src="../../assets/images/avatar.jpg" alt />
+            用户昵称
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped lang='less'>
+.warper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  .el-aside {
+    background-color: #002033;
+    .logo {
+      width: 100%;
+      height: 60px;
+      background: #002266 url(../../assets/images/logo_index.png) no-repeat
+        center / 120px auto;
+    }
+    .miniLogo {
+      background-image: url(../../assets/images/logo_admin_01.png);
+      background-size: 36px auto;
+    }
+    .el-menu {
+      border-right: none;
+    }
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .el-icon-s-fold {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown {
+      float: right;
+      img {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .el-dropdown-link {
+        font-weight: 700;
+      }
+    }
+  }
+}
 </style>
